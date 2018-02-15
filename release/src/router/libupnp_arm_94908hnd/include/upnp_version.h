98c110397a4b722b3c3b176c260c151e25caf9b8
Eric Sauvageau <rmerl@lostrealm.ca>
2018-02-15 00:02:22 -0500

    Merge HND and RT-AC86U binary blobs (GPL 384_20308)

--- release/src/router/libupnp_arm_94908hnd/include/upnp_version.h
+++ release/src/router/libupnp_arm_94908hnd/include/upnp_version.h
@@ -1,3 +1,16 @@
+/*
+ * Broadcom Proprietary and Confidential. Copyright (C) 2017,
+ * All Rights Reserved.
+ * 
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom;
+ * the contents of this file may not be disclosed to third parties, copied
+ * or duplicated in any form, in whole or in part, without the prior
+ * written permission of Broadcom.
+ *
+ * $Id: version.h.in,v 1.1 2010-02-11 04:08:13 $
+ *
+*/
+
 #ifndef __LIBUPNP_VERSION_H__
 #define __LIBUPNP_VERSION_H__
 
