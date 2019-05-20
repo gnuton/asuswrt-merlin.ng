a21ab40cc9741051581c224c824ddbd2045a202a
Antonio Aloisio <antonio.aloisio@zalando.fi>
2019-05-21 01:30:31 +0300

    webui: enabled firmware update checks for gnuton's builds

--- release/src/router/shared/defaults.c
+++ release/src/router/shared/defaults.c
@@ -3224,7 +3224,7 @@ struct nvram_tuple router_defaults[] = {
 #endif
 
 #ifdef RTCONFIG_MERLINUPDATE
-	{ "firmware_server", "https://fwupdate.asuswrt-merlin.net", CKN_STR128, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
+	{ "firmware_server", "https://gnuton.github.io/asuswrt-merlin.ng/updates", CKN_STR128, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 	{ "firmware_check_enable", "1", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 #endif
 
