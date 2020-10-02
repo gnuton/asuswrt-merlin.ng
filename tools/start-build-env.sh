b82d1c86b7af4cb5476c1f292f9dc58c12c0d2a0
Antonio Aloisio <antonio.aloisio@zalando.de>
2020-10-02 13:28:09 +0200

    start docker will start ubuntu 20_04 image

--- tools/start-build-env.sh
+++ tools/start-build-env.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run -it --rm -v "$PWD:/build" gnuton/asuswrt-merlin-toolchains-docker /bin/bash
+docker pull gnuton/asuswrt-merlin-toolchains-docker:latest-ubuntu-20_04
+docker run -it --rm -v "$PWD:/build" gnuton/asuswrt-merlin-toolchains-docker:latest-ubuntu-20_04 /bin/bash
 
