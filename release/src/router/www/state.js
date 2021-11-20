0233c7fbb22b85830862f9b0d0cf4acfdc8ec454
Antonio Aloisio <antonio.aloisio@zalando.de>
2021-11-20 15:22:21 +0100

    Revert "themes: Added themes support. Themes must be placed into /jffs/themes/my_theme and to enable it nvram set custom_theme=my_theme"
    This reverts commit 1b17a85cd57f7fbd4cae266d05ae0a9941630f1f.

--- release/src/router/www/state.js
+++ release/src/router/www/state.js
@@ -335,11 +335,7 @@ var SG_mode = ('<% nvram_get("SG_mode"); %>' == 1);
 
 var isGundam = in_territory_code("GD") || CoBrand_flag == 1;
 var isKimetsu = (CoBrand_flag == '2');
-var customTheme = '<% nvram_get("custom_theme"); %>';
-if (customTheme){
-    document.write('<link rel="stylesheet" type="text/css" href="/themes/' + customTheme + '/css/style.css"></link>');
-}
-else if(isGundam){
+if(isGundam){
 	document.write('<link rel="stylesheet" type="text/css" href="/css/gundam.css"></link>');
 }
 else if(isKimetsu){
