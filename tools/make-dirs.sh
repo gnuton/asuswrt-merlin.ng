bd48c60a380c6512ea1879df2e2dbab0788d5610
Antonio Aloisio <antonio.aloisio@zalando.de>
2022-02-24 00:44:33 +0100

    tools: Added helper tool for creating dirs and links

---
+++ tools/make-dirs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+cp -a $REPO/release/src/router/lighttpd-1.4.39/prebuild/RT-AX56U $REPO/release/src/router/lighttpd-1.4.39/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/httpd/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/rc/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/shared/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/sw-hw-auth/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/bwdpi_source/asus/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/bwdpi_source/asus_sql/prebuild/RT-AX56U $REPO/release/src/router/bwdpi_source/asus_sql/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/bwdpi_source/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/dropbox_client/prebuild/$BUILD_NAME
+
+cp -a $REPO/release/src/router/ftpclient/prebuild/RT-AX56U $REPO/release/src/router/ftpclient/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/protect_srv/prebuild/RT-AX56U $REPO/release/src/router/protect_srv/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/protect_srv/lib/prebuild/RT-AX56U $REPO/release/src/router/protect_srv/lib/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/sambaclient/prebuild/RT-AX56U $REPO/release/src/router/sambaclient/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/u2ec/prebuild/RT-AX56U $REPO/release/src/router/u2ec/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/wlc_nt/prebuild/RT-AX56U $REPO/release/src/router/wlc_nt/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/networkmap/prebuild/RT-AX56U $REPO/release/src/router/networkmap/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/nt_center/prebuild/RT-AX56U $REPO/release/src/router/nt_center/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/nt_center/actMail/prebuild/RT-AX56U $REPO/release/src/router/nt_center/actMail/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/nt_center/lib/prebuild/RT-AX56U $REPO/release/src/router/nt_center/lib/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/webdav_client/prebuild/RT-AX56U $REPO/release/src/router/webdav_client/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/usbclient/prebuild/RT-AX56U $REPO/release/src/router/usbclient/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/asuswebstorage/prebuild/RT-AX56U $REPO/release/src/router/asuswebstorage/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/inotify/prebuild/RT-AX56U $REPO/release/src/router/inotify/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/libletsencrypt/prebuild/RT-AX56U $REPO/release/src/router/libletsencrypt/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/libbcm/prebuilt/RT-AX56U $REPO/release/src/router/libbcm/prebuilt/$BUILD_NAME
+cp -a $REPO/release/src/router/infosvr/prebuild/RT-AX56U $REPO/release/src/router/infosvr/prebuild/$BUILD_NAME
+
+ls $REPO/release/src/router/ookla_speedtest/
+
+cp -a $REPO/release/src/router/dblog/daemon/prebuild/RT-AX56U $REPO/release/src/router/dblog/daemon/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/dblog/commands/prebuild/RT-AX56U $REPO/release/src/router/dblog/commands/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/ahs/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/asd/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/libasc/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/amas-utils/prebuild/$BUILD_NAME
+mkdir $REPO/release/src/router/cfg_mnt/prebuild/$BUILD_NAME
+
+cp -a $REPO/release/src/router/aaews/prebuild/RT-AX56U $REPO/release/src/router/aaews/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/asusnatnl/natnl/prebuild/RT-AX56U $REPO/release/src/router/asusnatnl/natnl/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/libasuslog/prebuild/RT-AX56U $REPO/release/src/router/libasuslog/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/sysstate/log_daemon/prebuild/RT-AX56U $REPO/release/src/router/sysstate/log_daemon/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/sysstate/commands/prebuild/RT-AX56U $REPO/release/src/router/sysstate/commands/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/wb/prebuild/RT-AX56U $REPO/release/src/router/wb/prebuild/$BUILD_NAME
+cp -a $REPO/release/src/router/eventd/prebuilt/RT-AX56U $REPO/release/src/router/eventd/prebuilt/$BUILD_NAME
+cp -a $REPO/release/src/router/bsd/prebuilt/RT-AX56U $REPO/release/src/router/bsd/prebuilt/$BUILD_NAME
