1c93b0c436844c41b849f5e713d1de799d24898c
Eric Sauvageau <merlin@asuswrt-merlin.net>
2022-03-30 16:54:49 -0400

    Merge SDK HND5.04 + GT-AX6000 router-sysdep
    Changes from original SDK:
    - cleaned bcmdrivers from object files.  Moved them to router-sysdep.MODEL_NAME
      to match other SDKs.
    - Instancied model-specific objects in bootloaders/

---
+++ release/src-rt-5.04axhnd.675x/bcmdrivers/opensource/phy/Serdes146Class/merlin16_shortfin_interface.h
@@ -0,0 +1,61 @@
+/*
+   Copyright (c) 2015 Broadcom Corporation
+   All Rights Reserved
+
+    <:label-BRCM:2015:DUAL/GPL:standard
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as published by
+    the Free Software Foundation (the "GPL").
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    
+    A copy of the GPL is available at http://www.broadcom.com/licenses/GPLv2.php, or by
+    writing to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+    
+:>
+*/
+/*! \mainpage
+ *
+ * \section intro_sec Introduction
+ *
+ * This documentation files are intended to guide a developer in using the
+ * Broadcom SerDes core within a larger ecosystem by providing specific API
+ * function calls which can be used to perform all necessary operations.
+ *
+ * The detailed description about the API directory structure, API overview,
+ * release procedure, and check out process can be found in the Serdes API User Guide
+ * (https://broadcom.box.com/v/merlin16_shortfin-common-16nm)
+ * \n \n
+ *
+ * \section API_sec Important API Files
+ *
+ * merlin16_shortfin_interface.h - provides the primary functionality.
+ *
+ * merlin16_shortfin_dependencies.h - defines the functions to be provided by the IP user
+ *
+ * \n \n
+ * Copyright: (c) 2019 Broadcom.                                                   *
+ * Broadcom Proprietary and Confidential. All rights reserved.                     *
+ */
+
+/** @file merlin16_shortfin_interface.h
+ * Main interface functions provided to IP User
+ */
+
+#ifndef MERLIN16_SHORTFIN_API_INTERFACE_H
+#define MERLIN16_SHORTFIN_API_INTERFACE_H
+
+#include "merlin16_shortfin_access.h"
+#include "merlin16_shortfin_config.h"
+#include "merlin16_shortfin_debug_functions.h"
+#include "merlin16_shortfin_diag.h"
+#include "merlin16_shortfin_prbs.h"
+#include "merlin16_shortfin_pwr_mgt.h"
+
+#endif
