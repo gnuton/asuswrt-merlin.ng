e8145c3950286c3f08edaf19352490300d41f3cf
Antonio Aloisio <antonio.aloisio@zalando.de>
2023-07-24 19:22:50 +0200

    tools: Updated tools/start-build-env.sh

--- tools/start-build-env.sh
+++ tools/start-build-env.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker pull gnuton/asuswrt-merlin-toolchains-docker:latest-ubuntu-20_04
-docker run -it --rm -v "$PWD:/build"  -u $(id -u ${USER}):$(id -g ${USER}) \
-       gnuton/asuswrt-merlin-toolchains-docker:latest-ubuntu-20_04 /bin/bash
+
+docker pull gnuton/asuswrt-merlin-toolchains-docker:latest
+docker run -it --rm -v "$PWD:/build"  -u "docker:docker" \
+       gnuton/asuswrt-merlin-toolchains-docker:latest /bin/bash
+
