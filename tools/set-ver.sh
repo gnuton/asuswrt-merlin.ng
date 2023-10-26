622f4124c9711d567d0f83a2d71d205e2361af9d
Antonio Aloisio <antonio.aloisio@zalando.de>
2023-10-26 09:42:34 +0200

    tools: fix set ver script

--- tools/set-ver.sh
+++ tools/set-ver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export CIRCLE_TAG=3004.388.4_0-gnuton0_beta1
-export MAJOR_VER=$(echo "${CIRCLE_TAG}" | sed -E 's/([0-9.]*)[_-]?(.*)/\1/')
-export MINOR_VER=$(echo "${CIRCLE_TAG}" | sed -E 's/([0-9.]*)[_-]?(.*)/\2/')
+export TAG=$(echo "${CIRCLE_TAG}" | sed 's/^[^.]*\.//')
+export MAJOR_VER=$(echo "${TAG}" | sed -E 's/([0-9.]*)[_-]?(.*)/\1/')
+export MINOR_VER=$(echo "${TAG}" | sed -E 's/([0-9.]*)[_-]?(.*)/\2/')
 sed -i "s/SERIALNO=.*/SERIALNO=${MAJOR_VER}/g" release/src-rt/version.conf
 sed -i "s/EXTENDNO=.*/EXTENDNO=${MINOR_VER}/g" release/src-rt/version.conf
 
