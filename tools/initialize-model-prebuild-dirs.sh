2b3ec181443f6621ed066a3892b5762397af97e6
Antonio Aloisio <antonio.aloisio@zalando.de>
2023-12-02 00:59:57 +0100

    Tools: removed old sdk symlink creation from tools/initialize-model-prebuild-dirs

--- tools/initialize-model-prebuild-dirs.sh
+++ tools/initialize-model-prebuild-dirs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-MODEL=RT-AXE95Q
-SDK=arm_6750hnd
-
-# Set to 1 to create NEW SDK directories
-CREATESDK=0
+MODEL=RT-AX5400
 
 cd ./release/src/router/
 
@@ -25,71 +21,36 @@ mkdir -p libasc/prebuild/$MODEL
 mkdir -p dns_ping/prebuild/$MODEL
 mkdir -p fsmd/prebuild/$MODEL
 
-if [ "$CREATESDK" = "1" ]; then
-# SDK dirs
-	mkdir -p aaews/prebuild/$SDK
-	mkdir -p asusnatnl/natnl/prebuild/$SDK
-	mkdir -p asuswebstorage/prebuild/$SDK
-	mkdir -p bsd/prebuilt/$SDK
-	mkdir -p dhd/prebuilt/$SDK
-	mkdir -p dropbox_client/prebuild/$SDK
-	mkdir -p eventd/prebuilt/$SDK
-	mkdir -p ftpclient/prebuild/$SDK
-	mkdir -p infosvr/prebuild/$SDK
-	mkdir -p inotify/prebuild/$SDK
-	mkdir -p libbcm/prebuilt/$SDK
-	mkdir -p libletsencrypt/prebuild/$SDK
-	mkdir -p lighttpd-1.4.39/prebuild/$SDK
-	mkdir -p networkmap/prebuild/$SDK
-	mkdir -p nt_center/actMail/prebuild/$SDK
-	mkdir -p nt_center/lib/prebuild/$SDK
-	mkdir -p nt_center/prebuild/$SDK
-	mkdir -p protect_srv/lib/prebuild/$SDK
-	mkdir -p protect_srv/prebuild/$SDK
-	mkdir -p sambaclient/prebuild/$SDK
-	mkdir -p sysstate/commands/prebuild/$SDK
-	mkdir -p sysstate/log_daemon/prebuild/$SDK
-	mkdir -p u2ec/prebuild/$SDK
-	mkdir -p usbclient/prebuild/$SDK
-	mkdir -p wb/prebuild/$SDK
-	mkdir -p webdav_client/prebuild/$SDK
-	mkdir -p wlc_nt/prebuild/$SDK
-	mkdir -p dblog/commands/prebuild/$SDK
-	mkdir -p dblog/daemon/prebuild/$SDK
-	mkdir -p libasuslog/prebuild/$SDK
-fi
+# Old MODEL dir in pre HND models
+mkdir -p aaews/prebuild/$MODEL
+mkdir -p asusnatnl/natnl/prebuild/$MODEL
+mkdir -p asuswebstorage/prebuild/$MODEL
+mkdir -p bsd/prebuilt/$MODEL
+mkdir -p dhd/prebuilt/$MODEL
+mkdir -p dropbox_client/prebuild/$MODEL
+mkdir -p eventd/prebuilt/$MODEL
+mkdir -p ftpclient/prebuild/$MODEL
+mkdir -p infosvr/prebuild/$MODEL
+mkdir -p inotify/prebuild/$MODEL
+mkdir -p libbcm/prebuilt/$MODEL
+mkdir -p libletsencrypt/prebuild/$MODEL
+mkdir -p lighttpd-1.4.39/prebuild/$MODEL
+mkdir -p networkmap/prebuild/$MODEL
+mkdir -p nt_center/actMail/prebuild/$MODEL
+mkdir -p nt_center/lib/prebuild/$MODEL
+mkdir -p nt_center/prebuild/$MODEL
+mkdir -p protect_srv/lib/prebuild/$MODEL
+mkdir -p protect_srv/prebuild/$MODEL
+mkdir -p sambaclient/prebuild/$MODEL
+mkdir -p sysstate/commands/prebuild/$MODEL
+mkdir -p sysstate/log_daemon/prebuild/$MODEL
+mkdir -p u2ec/prebuild/$MODEL
+mkdir -p usbclient/prebuild/$MODEL
+mkdir -p wb/prebuild/$MODEL
+mkdir -p webdav_client/prebuild/$MODEL
+mkdir -p wlc_nt/prebuild/$MODEL
+mkdir -p dblog/commands/prebuild/$MODEL
+mkdir -p dblog/daemon/prebuild/$MODEL
+mkdir -p libasuslog/prebuild/$MODEL
 
-# Links to SDK dirs
-ln -s $SDK aaews/prebuild/$MODEL
-ln -s $SDK asusnatnl/natnl/prebuild/$MODEL
-ln -s $SDK asuswebstorage/prebuild/$MODEL
-ln -s $SDK bsd/prebuilt/$MODEL
-ln -s $SDK dhd/prebuilt/$MODEL
-ln -s $SDK dropbox_client/prebuild/$MODEL
-ln -s $SDK eventd/prebuilt/$MODEL
-ln -s $SDK ftpclient/prebuild/$MODEL
-ln -s $SDK infosvr/prebuild/$MODEL
-ln -s $SDK inotify/prebuild/$MODEL
-ln -s $SDK libbcm/prebuilt/$MODEL
-ln -s $SDK libletsencrypt/prebuild/$MODEL
-ln -s $SDK lighttpd-1.4.39/prebuild/$MODEL
-ln -s $SDK networkmap/prebuild/$MODEL
-ln -s $SDK nt_center/actMail/prebuild/$MODEL
-ln -s $SDK nt_center/lib/prebuild/$MODEL
-ln -s $SDK nt_center/prebuild/$MODEL
-ln -s $SDK protect_srv/lib/prebuild/$MODEL
-ln -s $SDK protect_srv/prebuild/$MODEL
-ln -s $SDK sambaclient/prebuild/$MODEL
-ln -s $SDK sysstate/commands/prebuild/$MODEL
-ln -s $SDK sysstate/log_daemon/prebuild/$MODEL
-ln -s $SDK u2ec/prebuild/$MODEL
-ln -s $SDK usbclient/prebuild/$MODEL
-ln -s $SDK wb/prebuild/$MODEL
-ln -s $SDK webdav_client/prebuild/$MODEL
-ln -s $SDK wlc_nt/prebuild/$MODEL
-ln -s $SDK dblog/commands/prebuild/$MODEL
-ln -s $SDK dblog/daemon/prebuild/$MODEL
-ln -s $SDK libasuslog/prebuild/$MODEL
-
-
-echo "Don't forget to manually create release/SDK/hostTools/ and release/SDK/router-sysdep.MODEL"
+echo "Don't forget to manually create release/MODEL/hostTools/ and release/MODEL/router-sysdep.MODEL"
