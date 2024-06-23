ddac4a5d79fe0d2a64392c627f4d42ad24c0a816
Joel Samson <joel.samson@hotmail.com>
2024-06-23 15:53:31 -0400

    Update manifest2.txt to include RT-AX92U (#600)
    * Updating Notes and manifest to version:3004.388.7 1-gnuton1 [SKIP_CI]
    
    * Update manifest2.txt to include RT-AX92U
    
    Update manifest2.txt to include RT-AX92U
    
    * Update merlin_webs_update.sh
    
    * Update merlin_webs_update.sh
    
    * Update merlin_webs_update.sh
    
    * Update merlin_webs_update.sh
    
    * Update merlin_webs_update.sh
    
    ---------
    
    Co-authored-by: Circle CI BOT <bot@gnuton.org>

--- release/src/router/rom/webs_scripts/merlin_webs_update.sh
+++ release/src/router/rom/webs_scripts/merlin_webs_update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -u;
+
 wget_options="-q -t 2 -T 30"
 
-fwsite="https://raw.githubusercontent.com/gnuton/asuswrt-merlin.ng/master/updates/"
+fwsite="https://raw.githubusercontent.com/gnuton/asuswrt-merlin.ng/master/updates"
 
 nvram set webs_state_update=0 # INITIALIZING
 nvram set webs_state_flag=0   # 0: Don't do upgrade  1: Do upgrade
@@ -18,20 +18,15 @@ fi
 current_base=$(nvram get firmver | sed "s/\.//g")
 current_firm=$(nvram get buildno | cut -d. -f1)
 current_buildno=$(nvram get buildno | cut -d. -f2)
-current_extendno=$(nvram get extendno | sed "s/-g.*//" | sed "s/_.*//" | sed "s/alpha\|beta/-1/")
+#Extract extendno, subtract value by 1 if it contains "alpha/beta", remove all other values such as "_rog" or "-g*"
+current_extendno="$(nvram get extendno | awk -F'[_-]' '{n=$1} /[aA]lpha|[bB]eta/{n--} END{print n}')"
 
 # get firmware information
-forsq=$(nvram get apps_sq)
 model=$(nvram get productid)
 model="$model#"
 
-if [ "$forsq" == "1" ]; then
-	echo "---- update sq normal----" > /tmp/webs_upgrade.log
-	/usr/sbin/wget $wget_options $fwsite/test/manifest2.txt -O /tmp/wlan_update.txt
-else
-	echo "---- update real normal----" > /tmp/webs_upgrade.log
-	/usr/sbin/wget $wget_options $fwsite/manifest2.txt -O /tmp/wlan_update.txt
-fi
+echo "---- update real normal----" > /tmp/webs_upgrade.log
+/usr/sbin/wget $wget_options $fwsite/manifest2.txt -O /tmp/wlan_update.txt
 
 if [ "$?" != "0" ]; then
 	nvram set webs_state_error=1
@@ -88,21 +83,17 @@ fi
 webs_state_flag=$(nvram get webs_state_flag)
 
 if [ "$webs_state_flag" -eq "1" ]; then
-	releasenote_file0_US=$(nvram get webs_state_info)_note.txt
+	releasenote_file0=$(nvram get webs_state_info)_note.txt
 	releasenote_path0="/tmp/release_note0.txt"
-	if [ "$forsq" == "1" ]; then
-		echo "---- download SQ release note $fwsite/test/$releasenote_file0_US ----" >> /tmp/webs_upgrade.log
-		/usr/sbin/wget $wget_options $fwsite/test/$releasenote_file0_US -O $releasenote_path0
-		echo "---- $fwsite/test/$releasenote_file0 ----" >> /tmp/webs_upgrade.log
-	else
-		echo "---- download real release note ----" >> /tmp/webs_upgrade.log
-		/usr/sbin/wget $wget_options $fwsite/$releasenote_file0_US -O $releasenote_path0
-		echo "---- $fwsite/$releasenote_file0 ----" >> /tmp/webs_upgrade.log
-	fi
+	echo "---- download real release note ----" >> /tmp/webs_upgrade.log
+	/usr/sbin/wget $wget_options $fwsite/$releasenote_file0_US -O $releasenote_path0
+	echo "---- $fwsite/$releasenote_file0_US ----" >> /tmp/webs_upgrade.log
 
 	if [ "$?" != "0" ]; then
-		echo "---- download SQ release note failed ----" >> /tmp/webs_upgrade.log
+		echo "---- download $fwsite/$releasenote_file0 failed ----" >> /tmp/webs_upgrade.log
 		nvram set webs_state_error=1
+  	else
+		echo "---- $fwsite/$releasenote_file0 ----" >> /tmp/webs_upgrade.log
 	fi
 fi
 
